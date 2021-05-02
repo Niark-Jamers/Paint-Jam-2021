6a71b10e3d2822edffd06af18389ef9592fe2f45
bciss <bciss@student.42.fr>
2021-05-02 19:42:05 +0200

    electric lever

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,6 +9,7 @@ public class AudioManager : MonoBehaviour
 
     public AudioSource backgroundSource;
     public AudioSource sfxBackground;
+    public AudioSource pitch3;
 
     void Start()
     {
@@ -18,5 +19,9 @@ public class AudioManager : MonoBehaviour
     public void PlaySFX(AudioClip clip, float volume = 1)
     {
         sfxBackground.PlayOneShot(clip, volume);
+    }
+    public void PlaySFXPitch3(AudioClip clip, float volume = 1)
+    {
+        pitch3.PlayOneShot(clip, volume);
     }
 }
